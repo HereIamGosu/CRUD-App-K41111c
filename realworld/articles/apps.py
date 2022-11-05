990e32d7f4d8162244d69da70c7ccb23f66bd281
HereIamGosu <dav2099@gmail.com>
2022-11-05 17:40:14 +0300

    4th try

--- realworld/articles/apps.py
+++ realworld/articles/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ArticlesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "gosu.articles"
+    name = "realworld.articles"
--- realworld/urls.py
+++ realworld/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("gosu.articles.urls")),
-    path("", include("gosu.accounts.urls")),
-    path("comments/", include("gosu.comments.urls")),
+    path("", include("realworld.articles.urls")),
+    path("", include("realworld.accounts.urls")),
+    path("comments/", include("realworld.comments.urls")),
     path("admin/", admin.site.urls),
 ]
